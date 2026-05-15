fe6785fecaf82c1b9a00733a16e2acde24edde26
Levis254 <gichukilevis20@gmail.com>
2026-05-15 09:48:12 +0300

    Add Retrofit client configuration for M-Pesa API

---
+++ app/src/main/java/com/shop/globalmarket/data/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.shop.globalmarket.data
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://sandbox.safaricom.co.ke/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
+        .build()
+
+    val mpesaApiService: MpesaApiService = retrofit.create(MpesaApiService::class.java)
+}
